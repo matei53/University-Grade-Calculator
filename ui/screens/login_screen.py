0e9a9b4d43524d61f7a71ed4f2508a0573fb4f01
Matt <122106113+matei53@users.noreply.github.com>
2026-05-05 13:01:03 +0300

    updated style of login and signup screens

--- ui/screens/login_screen.py
+++ ui/screens/login_screen.py
@@ -5,47 +5,68 @@ from PyQt6.QtWidgets import (
 from PyQt6.QtCore import Qt
 from services.auth_service import AuthService
 from models.session import Session
+from ui.styles import AUTH_STYLE
 
 class LoginScreen(QWidget):
     def __init__(self, router):
         super().__init__()
         self.router = router
         self.auth_service = AuthService()
+        self.setStyleSheet(AUTH_STYLE)
         self._build_ui()
 
     def _build_ui(self):
-        layout = QVBoxLayout()
-        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layout.setSpacing(12)
-        self.setLayout(layout)
+        self.main_layout = QVBoxLayout(self)
+        self.main_layout.setContentsMargins(40, 40, 40, 40)
+        self.main_layout.setSpacing(20)
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        layout.addWidget(QLabel("<h2>Grade Tracker</h2>"))
+        # Title
+        title = QLabel("UniGrade")
+        title.setObjectName("AuthTitle")
+        self.main_layout.addWidget(title, alignment=Qt.AlignmentFlag.AlignCenter)
 
+        subtitle = QLabel("Log In")
+        subtitle.setObjectName("AuthSubtitle")
+        self.main_layout.addWidget(subtitle, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        self.main_layout.addSpacing(20)
+
+        # Input fields
         self.username_input = QLineEdit()
         self.username_input.setPlaceholderText("Username")
+        self.username_input.setObjectName("AuthInput")
         self.username_input.setFixedWidth(300)
 
         self.password_input = QLineEdit()
         self.password_input.setPlaceholderText("Password")
         self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
+        self.password_input.setObjectName("AuthInput")
         self.password_input.setFixedWidth(300)
 
+        self.main_layout.addWidget(self.username_input, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.main_layout.addWidget(self.password_input, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        # Error label
+        self.error_label = QLabel("")
+        self.error_label.setObjectName("ErrorLabel")
+        self.main_layout.addWidget(self.error_label, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        self.main_layout.addSpacing(10)
+
+        # Buttons
         login_btn = QPushButton("Log In")
+        login_btn.setObjectName("PrimaryButton")
         login_btn.setFixedWidth(300)
         login_btn.clicked.connect(self._handle_login)
 
         signup_btn = QPushButton("Don't have an account? Sign Up")
-        signup_btn.setFlat(True)
+        signup_btn.setObjectName("SecondaryLink")
         signup_btn.clicked.connect(lambda: self.router.navigate("signup"))
 
-        self.error_label = QLabel("")
-        self.error_label.setStyleSheet("color: red;")
-
-        layout.addWidget(self.username_input, alignment=Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(self.password_input, alignment=Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(self.error_label, alignment=Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(login_btn, alignment=Qt.AlignmentFlag.AlignCenter)
-        layout.addWidget(signup_btn, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.main_layout.addWidget(login_btn, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.main_layout.addWidget(signup_btn, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.main_layout.addStretch()
 
     def _handle_login(self):
         username = self.username_input.text().strip()
--- ui/screens/signup_screen.py
+++ ui/screens/signup_screen.py
@@ -8,34 +8,49 @@ from services.auth_service import AuthService
 from models.session import Session
 from ui.components.university_picker import UniversityPicker
 from ui.components.major_picker import MajorPicker
+from ui.styles import AUTH_STYLE
 
 class SignupScreen(QWidget):
     def __init__(self, router):
         super().__init__()
         self.router = router
         self.auth_service = AuthService()
+        self.setStyleSheet(AUTH_STYLE)
         self._build_ui()
 
     def _build_ui(self):
-        layout = QVBoxLayout()
-        layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
-        layout.setSpacing(12)
-        self.setLayout(layout)
+        self.main_layout = QVBoxLayout(self)
+        self.main_layout.setContentsMargins(40, 40, 40, 40)
+        self.main_layout.setSpacing(15)
+        self.main_layout.setAlignment(Qt.AlignmentFlag.AlignCenter)
 
-        layout.addWidget(QLabel("<h2>Create Account</h2>"))
+        # Title
+        title = QLabel("UniGrade")
+        title.setObjectName("AuthTitle")
+        self.main_layout.addWidget(title, alignment=Qt.AlignmentFlag.AlignCenter)
 
+        subtitle = QLabel("Create Account")
+        subtitle.setObjectName("AuthSubtitle")
+        self.main_layout.addWidget(subtitle, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        self.main_layout.addSpacing(20)
+
+        # Input fields
         self.username_input = QLineEdit()
         self.username_input.setPlaceholderText("Username")
+        self.username_input.setObjectName("AuthInput")
         self.username_input.setFixedWidth(300)
 
         self.password_input = QLineEdit()
         self.password_input.setPlaceholderText("Password")
         self.password_input.setEchoMode(QLineEdit.EchoMode.Password)
+        self.password_input.setObjectName("AuthInput")
         self.password_input.setFixedWidth(300)
 
         self.confirm_input = QLineEdit()
         self.confirm_input.setPlaceholderText("Confirm Password")
         self.confirm_input.setEchoMode(QLineEdit.EchoMode.Password)
+        self.confirm_input.setObjectName("AuthInput")
         self.confirm_input.setFixedWidth(300)
 
         self.university_picker = UniversityPicker()
@@ -44,24 +59,34 @@ class SignupScreen(QWidget):
         self.major_picker = MajorPicker()
         self.major_picker.setFixedWidth(300)
 
+        self.main_layout.addWidget(self.username_input, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.main_layout.addWidget(self.password_input, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.main_layout.addWidget(self.confirm_input, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.main_layout.addWidget(self.university_picker, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.main_layout.addWidget(self.major_picker, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        self.main_layout.addSpacing(10)
+
+        # Error label
+        self.error_label = QLabel("")
+        self.error_label.setObjectName("ErrorLabel")
+        self.main_layout.addWidget(self.error_label, alignment=Qt.AlignmentFlag.AlignCenter)
+
+        self.main_layout.addSpacing(10)
+
+        # Buttons
         signup_btn = QPushButton("Sign Up")
+        signup_btn.setObjectName("PrimaryButton")
         signup_btn.setFixedWidth(300)
         signup_btn.clicked.connect(self._handle_signup)
 
         back_btn = QPushButton("Already have an account? Log In")
-        back_btn.setFlat(True)
+        back_btn.setObjectName("SecondaryLink")
         back_btn.clicked.connect(lambda: self.router.navigate("login"))
 
-        self.error_label = QLabel("")
-        self.error_label.setStyleSheet("color: red;")
-
-        for widget in [
-            self.username_input, self.password_input,
-            self.confirm_input, self.university_picker,
-            self.major_picker, self.error_label,
-            signup_btn, back_btn
-        ]:
-            layout.addWidget(widget, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.main_layout.addWidget(signup_btn, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.main_layout.addWidget(back_btn, alignment=Qt.AlignmentFlag.AlignCenter)
+        self.main_layout.addStretch()
 
     def _handle_signup(self):
         username = self.username_input.text().strip()
